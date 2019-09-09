51015e4544884eabcc6f6de5547956df4abbdf3e
rmjones112 <rmjones112gmail.com>
2019-09-09 00:36:19 -0700

    javascript, html and css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,103 @@
 //when timer runs out reveal answers
 //questions need t/f answers / multiple choice answers
 
-//start game 
-$(document).ready(function() {
-    
+
+    //defining onclick functions that run in the window
+window.onload = function() {
+    $("#start-button").on("click", stopwatch.start);
+    $(".choice").on("click", stopwatch.correctCount);
+    $("#done-button").on("click", stopwatch.stop);
+    $("#restart-button").on("click", stopwatch.reset);
+};
+
+//creating the initial glabal variables
+var intervalId;
+var clockRunning = false;
+var correctAnswers = 0;
+
+// stopwatch object which contains the clock functions
+var stopwatch = {
+
+    // setting clock to 1:30 aka 90 seconds 
+    time: 90,
+
+    // function that checks for correct answers and updates score
+    correctCount: function() {
+        
+        //checks the selection that is clicked on for the value "correct"
+        if (clockRunning) {
+            var selection = $(this).val().trim();
+            if (selection === "correct" && correctAnswers < 8) {
+                correctAnswers++
+            }
+
+            //if player gets all 8 questions correct, the game ends
+            else if (correctAnswers > 8) {
+                stopwatch.stop();
+            }
+        }
+
+    },
+
+    //reset function 
+    reset: function() {
+
+        //the time and score reset, clock stops, and choices are deselected
+        stopwatch.stop();
+        stopwatch.time = 90;
+        correctAnswers = 0;
+        $("#time-left").text("1:30");
+        $("input[type='radio']").prop('checked', false);
+    },
+
+    //function that starts the countdown clock
+    start: function() {
+        if (!clockRunning) {
+            intervalId = setInterval(stopwatch.count, 1000);
+            clockRunning = true;
+        }
+    },
+
+    //function that stops the countdown clock and ends the game
+    stop: function() {
+        clearInterval(intervalId);
+        clockRunning = false;
+
+        //instead of the remaining time, the score is displayed in the header
+        $("#time-left").html("Score:" + correctAnswers + "/8");
+    },
+
+    //function initialized in the start function, counts down in intervals of 1000 ms
+    count: function() {
+
+        //as long as there is still time left on the clock, keep counting down
+        if (stopwatch.time > 0) {
+            stopwatch.time--;
+            var converted = stopwatch.timeConverter(stopwatch.time);
+
+            //time remaining displays in the header
+            $("#time-left").text(converted);
+        }
+
+        //clock stops at 0
+        else {
+            stopwatch.stop();
+        }
+    },
+
+    // Had to make function so time displayed in mins/sec
+    timeConverter: function(t) {
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes === 0) {
+            minutes = "00";
+        }
+        else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        return minutes + ":" + seconds;
+        }
+};   
